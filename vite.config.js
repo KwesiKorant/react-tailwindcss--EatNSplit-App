e220b5b5ed4f009a71e54abfdd472143442a37f2
Kwesi Korant <junykorant@gmail.com>
2026-04-02 21:51:32 +0000

    2nd commit - updated package.json and vite.config.js to automatically open browser on 'npm run dev'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  
+  server: {
+    open: true, // This will open the browser automatically
+  },
+
   plugins: [
     tailwindcss(),
     react({
